97e2ce1ff8ad5315344ad1e88ca5c63d80385b10
Maximilian <scientificresearch309@gmail.com>
2022-11-10 17:14:25 +0100

    Project completed!

--- main.js
+++ main.js
@@ -1,86 +1,61 @@
-const api = {
-    key: "e7a45e87530228a70b668edd89af3109",
-    base: "https://api.openweathermap.org/data/2.5/",
-  };
-  
-  const searchbox = document.querySelector(".search-box");
-  searchbox.addEventListener("keypress", setQuery);
-  
-  function setQuery(evt) {
-    if (evt.keyCode == 13) {
-      getResults(searchbox.value);
-      console.log(searchbox.value);
+//const apiFlight = {
+//base: "http://api.aviationstack.com/v1/",
+// base: "http://api.aviationstack.com/v1/flights?access_key=0fb51295b712b9c0be26db517e285b7b",
+//key: "074aec8cf4db2c3c0868d568a990b75e",
+//};
+
+let params = new URLSearchParams({
+  access_key: "074aec8cf4db2c3c0868d568a990b75e",
+  limit: 7,
+});
+const getFlight = () => {
+  fetch(`http://api.aviationstack.com/v1/flights?${params}`)
+    .then((flights) => {
+      return flights.json();
+    })
+    .then((flights) => {
+      console.log(flights);
+      populateTable(flights);
+    })
+    .catch((err) => console.log(err));
+};
+
+getFlight();
+
+const tableBody = document.getElementById("table-body");
+
+const populateTable = (flights) => {
+  for (const flight of flights.data) {
+    const tableRow = document.createElement("tr");
+    const tableIcon = document.createElement("td");
+    // tableIcon.textContent = "https://www.flaticon.com/free-icon/airplane_7118033";
+    //tableIcon.textContent = image;
+    tableRow.append(tableIcon);
+
+    const flightDetails = {
+      airline: flight.airline.name.toUpperCase(),
+      arrival: flight.arrival.airport.toUpperCase(),
+      departure: flight.departure.airport.toUpperCase(),
+      flight: flight.flight.number,
+      date: flight.flight_date,
+      status: flight.flight_status.toUpperCase(),
+    };
+
+    for (const flightDetail in flightDetails) {
+      const tableCell = document.createElement("td");
+      const word = Array.from(flightDetails[flightDetail]);
+
+      for (const [index, letter] of word.entries()) {
+        const letterElement = document.createElement("div");
+
+        setTimeout(() => {
+          letterElement.classList.add("flip");
+          letterElement.textContent = letter;
+          tableCell.append(letterElement);
+        }, 100 * index);
+      }
+      tableRow.append(tableCell);
     }
+    tableBody.append(tableRow);
   }
-  
-  function getResults(query) {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then((weather) => {
-        return weather.json();
-      })
-      .then(displayResults);
-  }
-  
-  function displayResults(weather) {
-    console.log(weather);
-    let city = document.querySelector(".location .city");
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
-  
-    let now = new Date();
-    let date = document.querySelector(".location .date");
-    date.innerHTML = dateBuilder(now);
-  
-    let temp = document.querySelector(".current .temp");
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-  
-    let weather_el = document.querySelector(".current .weather");
-    weather_el.innerHTML = weather.weather[0].main;
-  
-    let icon_el = document.querySelector(".current .icon");
-    //icon_el.innerHTML = weather.weather[0].icon;
-    icon_el.innerHTML = `<img src="icons/${weather.weather[0].icon}.png"/>`;
-  
-    let hilow = document.querySelector(".hi-low");
-    hilow.innerHTML = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
-      weather.main.temp_max
-    )}°c`;
-  }
-  
-  function dateBuilder(d) {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-  
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-  
-    return `${day} ${date} ${month} ${year}`;
-  }
-  
-  // function setIcons(icon,iconID){
-  //  const skycons = new skycons({color:"white"});
-  //  const currentIcon =
-  // }
-  
+};
